36e0e7ad195a5676bd74d9e82876e42377778142
Dominic Nguyen <canh.nguyen@magnolia-cms.com>
2020-04-16 11:37:45 +0700

    Reset status

--- src/Components/Servers/Server.vue
+++ src/Components/Servers/Server.vue
@@ -2,6 +2,7 @@
   <div>
       <p v-if="!server">Server Details are currently not updated</p>
       <p v-else>Server #{{ server.id }}: {{ server.status }}</p>
+      <button class="btn btn-default" @click="resetStatus">Reset Status</button>
   </div>
 </template>
 
@@ -14,7 +15,9 @@ export default {
         };
     },
     methods: {
-        
+        resetStatus() {
+            this.server.status = 'Normal';
+        }
     },
     created() {
         eventBus.$on('serverSelected', server => {
